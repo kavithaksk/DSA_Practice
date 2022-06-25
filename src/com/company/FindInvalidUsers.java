6447e1b04f258478543695d4c3979e272ce2dce4
kavitha_ksk <kavitha.ksk@gmail.com>
2022-06-25 15:35:22 -0400

    Few more problems

---
+++ src/com/company/FindInvalidUsers.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import java.util.*;
+
+public class FindInvalidUsers {
+
+    public static String[][]  badge_records =   new String[][]
+            {
+                    { "Martha", "exit"},
+                    { "Paul", "enter"},
+                    { "Martha", "enter"},
+                    { "Martha", "exit"},
+                    { "Jennifer", "enter"},
+                    { "Paul", "enter"},
+                    { "Curtis", "enter"},
+                    { "Paul", "exit"},
+                    { "Martha", "enter"},
+                    { "Martha", "exit"},
+                    { "Jennifer", "exit"},
+            };
+
+    public static void findInvalidUsers()
+    {
+        /* create a map to store the entry and exit values. When there is entry, set it to 1 and when there is exit, then set it to 0 */
+        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> noEntryMap = new HashSet<>();
+        HashSet<String> noExitMap = new HashSet<>();
+        for (int i  = 0; i < badge_records.length; i++)
+        {
+            String[] s = badge_records[i];
+            if (s[1].equals("exit"))
+            {
+                int count = map.getOrDefault(s[0], 0);
+                if (count != 1)
+                    noEntryMap.add(s[0]);
+                else
+                {
+                    map.put(s[0], 0);
+                }
+            }
+
+            else if (s[1].equals("enter")) {
+                int count = map.getOrDefault(s[0], 0);
+                if (count != 1)
+                    map.put(s[0], 1);
+                else
+                    noExitMap.add(s[0]);
+            }
+
+
+        }
+
+        for (Map.Entry<String, Integer> kv : map.entrySet())
+        {
+            if (kv.getValue() != 0) {
+                if (!noExitMap.contains(kv.getKey()))
+                    noExitMap.add(kv.getKey());
+            }
+        }
+
+        List<List<String>> invalidEntries = new ArrayList<>();
+        List<String> invalidExitValues = new ArrayList<>(noExitMap);
+        List<String> invalidEntryValues = new ArrayList<>(noEntryMap);
+
+        invalidEntries.add(invalidExitValues);
+        invalidEntries.add(invalidEntryValues);
+
+        invalidEntries.stream().forEach(s-> System.out.println(s));
+
+    }
+
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -44,35 +44,37 @@ public class Main {
 
         AdsConversionRate.getAdsConversionRate();
 
+        FindInvalidUsers.findInvalidUsers();
 
 
-        int[][] grid = new int[][]{
-            { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0,1,0,0,1,1,0,0,1,0,1,0,0}, {0,1,0,0,1,1,0,0,1,1,1,0,0}, {0,0,0,0,0,0,0,0,0,0,1,0,0}, {0,0,0,0,0,0,0,1,1,1,0,0,0},
-                {0,0,0,0,0,0,0,1,1,0,0,0,0}};
-
-        //interviewQuestions.MaxAreaIsland(grid, 104);
-        int missingNumber = FindFirstMissingPositiveNumber.findFirstMissingPositive();
-        System.out.println("Missing positive number is : " + missingNumber);
-
-        int[] candidates = new int[] {10,1,2,7,6,1,5};
-        CombinationsSum.CombinationSumII(candidates, 8);
-        int result = interviewQuestions.myAtoi("   -42");
-        System.out.println(result);
-        int longestRepeatingCount = LengthOfLongestNonRepeatingChar.lengthOfLongestNonRepeatingChar();
-        System.out.println("Length of longest substring without repeating char's -->" +longestRepeatingCount);
-
-        int arr[] = new int[] { 142,112,54,69,148,45,63,158,38,60,124,142,130,179,117,36,191,43,89,107,41,143,65,49,47,6,91,130,171,151,7,102,194,149,30,24,85,155,157,41,167,177,132,109,145,40,27,124,138,139,119,83,130,142,34,116,40,59,105,131,178,107,74,187,22,146,125,73,71,30,178,174,98,113};
-        System.out.println(arr.length);
-        List<Integer> result1 = interviewQuestions.findSubArrayWithGivenSum(arr,74, 665 );
-        result1.stream().forEach(System.out::println);
-        Stream.of(result1.toArray()).forEach(System.out::println);
-
-        int input[] = new int[] {0, 1, 2, 99, 13, 23, 4, 6, 8, 3, 5, 8, 9, 11, 10, 12, 13, 16};
-        interviewQuestions.reArrangeEvenAndOdd(input);
-
-        Integer[] inputArray = {-10, -3, 5, 6, -2};
-        List<Integer> maxProduct = interviewQuestions.findMaxiumProductofTwoIntegersInArray(inputArray);
+//
+//        int[][] grid = new int[][]{
+//            { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0,1,0,0,1,1,0,0,1,0,1,0,0}, {0,1,0,0,1,1,0,0,1,1,1,0,0}, {0,0,0,0,0,0,0,0,0,0,1,0,0}, {0,0,0,0,0,0,0,1,1,1,0,0,0},
+//                {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+//
+//        //interviewQuestions.MaxAreaIsland(grid, 104);
+//        int missingNumber = FindFirstMissingPositiveNumber.findFirstMissingPositive();
+//        System.out.println("Missing positive number is : " + missingNumber);
+//
+//        int[] candidates = new int[] {10,1,2,7,6,1,5};
+//        CombinationsSum.CombinationSumII(candidates, 8);
+//        int result = interviewQuestions.myAtoi("   -42");
+//        System.out.println(result);
+//        int longestRepeatingCount = LengthOfLongestNonRepeatingChar.lengthOfLongestNonRepeatingChar();
+//        System.out.println("Length of longest substring without repeating char's -->" +longestRepeatingCount);
+//
+//        int arr[] = new int[] { 142,112,54,69,148,45,63,158,38,60,124,142,130,179,117,36,191,43,89,107,41,143,65,49,47,6,91,130,171,151,7,102,194,149,30,24,85,155,157,41,167,177,132,109,145,40,27,124,138,139,119,83,130,142,34,116,40,59,105,131,178,107,74,187,22,146,125,73,71,30,178,174,98,113};
+//        System.out.println(arr.length);
+//        List<Integer> result1 = interviewQuestions.findSubArrayWithGivenSum(arr,74, 665 );
+//        result1.stream().forEach(System.out::println);
+//        Stream.of(result1.toArray()).forEach(System.out::println);
+//
+//        int input[] = new int[] {0, 1, 2, 99, 13, 23, 4, 6, 8, 3, 5, 8, 9, 11, 10, 12, 13, 16};
+//        interviewQuestions.reArrangeEvenAndOdd(input);
+//
+//        Integer[] inputArray = {-10, -3, 5, 6, -2};
+//        List<Integer> maxProduct = interviewQuestions.findMaxiumProductofTwoIntegersInArray(inputArray);
 
         //interviewQuestions.convertIntegerToBinary(Integer.MAX_VALUE);
 
